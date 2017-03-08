4a5784ef8f432ce992898d48601abda28f4a2d2f
unnamed42 <a390517426@gmail.com>
2017-03-08 18:11:34 +0800

    Site updated: 2017-03-08 18:11:34

---
+++ js/spoiler.js
@@ -0,0 +1,8 @@
+(function(){
+    var spoiler = document.getElementsByClassName("spoiler");
+    for(var i = 0; i < spoiler.length; ++i) {
+        spoiler[i].addEventListener("click", function() {
+            this.classList.toggle("revealed");
+        });
+    }
+})();
